df74d8deb73aaa733f952e7fd6f13488c2eccc59
Tom Meadon <tom@meadon.me>
2021-11-06 08:32:03 +0000

    app change

--- apps/frontend/src/App.js
+++ apps/frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Azure Container Apps playground!
         </p>
         <a
           className="App-link"
